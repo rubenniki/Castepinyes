e08988b30a72a18853fa74d0472888b0240144b9
rubenniki <rubenniki@gmail.com>
2018-03-09 15:10:58 +0100

    permisos manifest

--- app/src/main/java/com/ruben/castepinyes/MostrarFotoPinyaFragment.java
+++ app/src/main/java/com/ruben/castepinyes/MostrarFotoPinyaFragment.java
@@ -64,7 +64,12 @@ public class MostrarFotoPinyaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mostrar_foto_pinya, container, false);
+        View view=inflater.inflate(R.layout.fragment_mostrar_foto_pinya, container, false);
+
+
+
+        return view;
+
     }
     
     /**
