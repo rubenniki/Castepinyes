fbb456c72dfa3a6afd513dcad22f07e734f9a9cc
rubenniki <rubenniki@gmail.com>
2018-04-11 16:43:12 +0200

    fotos

---
+++ app/src/main/java/com/ruben/castepinyes/Adapter_foto_listView.java
@@ -0,0 +1,61 @@
+package com.ruben.castepinyes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ruben on 16/03/2018.
+ */
+
+public class Adapter_foto_listView extends BaseAdapter{
+
+    public ArrayList imagen;
+    public Context context;
+
+    public Adapter_foto_listView(ArrayList imagen, Context context) {
+        this.imagen = imagen;
+        this.context = context;
+    }
+
+    private static class ViewHolder {
+
+        public ImageView imagen;
+    }
+
+    @Override
+    public int getCount() {
+        return imagen.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return imagen.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.adapter_fotos_pinya, parent, false);
+
+            viewHolder = new ViewHolder();
+
+            viewHolder.imagen=convertView.findViewById(R.id.Foto_pinya);
+
+            Picasso.get().load(imagen.get(position)).into(viewHolder.imagen);
+
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/ruben/castepinyes/Fotos_Colla.java
@@ -0,0 +1,29 @@
+package com.ruben.castepinyes;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fotos_Colla extends Fragment {
+
+
+    public Fotos_Colla() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fotos__colla, container, false);
+    }
+
+}
